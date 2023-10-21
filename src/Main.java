92a2a3c499956610c938bfd78a27e3c7d6b20359
Muhammad <auwalmuhd365@gmail.com>
2023-10-21 11:45:21 +0100

    implemented basic features

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
 
 public class Main {
+    
     public static void main(String[] args) {
-
+        
         Scanner scanner = new Scanner(System.in);
         boolean isRunning = false;
 
@@ -11,9 +12,64 @@ public class Main {
         int startChoice = scanner.nextInt();
 
         if(startChoice == 1){
-            isRunning = true;
-//            startApp();
+            isRunning = true; 
+        }
+        
+        while(isRunning){
+            isRunning = startApp();
         }
 
+        // Task task = new Task("My first task");
+        // task.addTask("Wake up");        
+        // task.addTask("Sleep");
+        // task.addTask("Eat");
+
+        // System.out.println(
+        //     task.toString()
+        // );
+
+    }
+
+    static boolean startApp(){
+
+
+        Scanner scanner = new Scanner(System.in);
+        Task task;
+
+        System.out.println("Welcome!");
+        System.out.println("Choose an option");
+        System.out.println("1. Create task");
+        System.out.println("2. Add a task");
+        System.out.println("3. List all tasks");
+        System.out.println("4. Remove a task");
+        System.out.println("5. Quit application");
+
+        int choice = scanner.nextInt();
+
+        switch(choice){
+            case 1: 
+                task = createTask();
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                System.out.println("Bye!");
+                return false;
+        }
+        return true;
+    }
+
+    static Task createTask(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter Task name: ");
+        String name = scanner.nextLine();
+
+        Task task = new Task(name);
+        return task;
     }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,8 +1,59 @@
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Task {
     private String name;
-    private Date created;
+    private LocalDate dateCreated;
     private String description;
-    private String status;
+    private int numOfTask = 0;
+
+    // Constructors
+    Task(){
+        this.name = "Task";        
+        this.dateCreated = LocalDate.now();
+        this.description = "";
+    }
+    Task(String name){
+        this.name = name;
+        this.dateCreated = LocalDate.now();
+        this.description = "";
+
+    }    
+    Task(String name, String description){
+        this.name = name;
+        this.dateCreated = LocalDate.now();
+        this.description = description;
+    }
+
+    // override toString method
+    @Override
+    public String toString(){
+        return (
+            this.name + "\n" 
+            + this.dateCreated + "\n" 
+            + this.description + "\n" 
+        );
+    }
+
+    // Getters
+    public String getDescription() {
+        return description;
+    }
+    public String getName() {
+        return name;
+    }
+
+    // Setters
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Task manipulation
+    public void addTask(String description){
+        ++numOfTask;
+        this.description += "\n" + numOfTask + ". " + description;
+    }
+
 }
